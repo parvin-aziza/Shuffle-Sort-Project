65eda80b7b6216550e3b5569ea245072807312fe
parvin-aziza <azizaparvin07@gmail.com>
2022-11-01 13:55:42 +0530

    Added Shuffle and Sort Functionality

--- src/shuffleSort.js
+++ src/shuffleSort.js
@@ -5,3 +5,35 @@ let arrNumber = [1,2,3,4,5,6,7,8,9];
 arrNumber.map((number) => {
     box.innerHTML += '<li data-item=' + number + '>' + number + '</li>'
 })
+
+//Generic Method to add list
+const showListOfNumbers=(data)=>{
+  box.innerHTML = ''
+  for(let i=0;i<arrNumber.length;i++){
+      let listItem = document.createElement("li");
+      listItem.innerHTML = arrNumber[i]
+      listItem.setAttribute('data-item', arrNumber[i])
+      box.appendChild(listItem)
+  }
+}
+//Shuffle Array Method
+const getShuffledNumbers=(numbersArrayItems) => {
+  arrNumber = numbersArrayItems;
+  for (let i = arrNumber.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arrNumber[i], arrNumber[j]] = [arrNumber[j], arrNumber[i]];
+  }
+}
+
+//To Shuffle Numbers onClick of Shuffle Button 
+const handleShuffle = () => {
+  getShuffledNumbers(arrNumber);
+  showListOfNumbers(arrNumber);
+}
+
+//To Shuffle Numbers onClick of Sort Button 
+const handleSort = () => {
+arrNumber = arrNumber.sort((a, b) => { return a - b });
+showListOfNumbers(arrNumber);
+}
+
